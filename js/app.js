dc681e83dd81e9bc3ba5ad4cb515de63dc5f13f7
mahamudaNaznin <143586556+mahamudaNaznin@users.noreply.github.com>
2023-11-05 17:02:47 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+Grade Calculator
+Odd & Even Number
+
+let num1 = 2;
+let num2 = 5;
+document.write(num1 + num2 + "<br>");
+
+let num3 = 20;
+let num4 = 50;
+document.write(num3 + num4 + "<br>");
+
+
+
+
+
+// function sum(a,b){
+//     let num5 = a;
+//     let num6 = b;
+//     return num5 + num6;
+// }
+// console.log(sum(20,20) + 20)
